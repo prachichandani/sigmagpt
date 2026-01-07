8075908f55a2023c78808e1c8318427c484d5faf
prachi chandani <prachichandani05@gmail.com>
2026-01-07 15:28:03 +0530

    Fix cors for vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://sigmagpt-phi.vercel.app/","http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
